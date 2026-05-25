57313d6af1198ce31fb9e7d116e144cfcd2ab816
d4ngv0-vn <d4ngv01303@gmail.com>
2026-05-25 21:42:48 +0700

    building nn framework

---
+++ nn.h
@@ -0,0 +1,157 @@
+#ifndef NN_H_
+#define NN_H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <math.h>
+
+#ifndef NN_MALLOC
+#include <stdlib.h>
+#define NN_MALLOC malloc
+#endif // NN_MALLOC
+
+#ifndef NN_ASSERT
+#include <assert.h>
+#define NN_ASSERT assert
+#endif // NN_ASSERT
+
+typedef struct {
+    size_t rows;
+    size_t cols;
+    size_t stride;
+    float *es;
+} Mat;
+
+#define MAT_AT(m, i, j) (m).es[(i)*(m).stride + (j)]
+
+float rand_float(void);
+float sigmoidf(float x);
+
+//allocate matrix with sizeof(*es)*rows*cols
+Mat mat_alloc(size_t rows, size_t cols);
+//random m
+void mat_rand(Mat m, float low, float high);
+//fill m with a
+void mat_fill(Mat m, float x);
+//dst = a * b
+void mat_dot(Mat dst, Mat a, Mat b);
+//dst = dst + a
+void mat_sum(Mat dst, Mat a);
+//print m with its name
+void mat_print(Mat m, const char *name);
+//activate m with sigmoid
+void mat_sig(Mat m);
+//return the row of the matrix
+Mat mat_row(Mat m, size_t row);
+void mat_col(Mat m, size_t col);
+//copy src to dst
+void mat_copy(Mat dst, Mat src);
+
+#define MAT_PRINT(m) mat_print(m, #m)
+
+
+#endif // NN_H_
+
+#ifdef NN_IMPLEMENTATION
+
+float rand_float(void) {
+    return (float) rand() / (float) RAND_MAX;
+}
+
+float sigmoidf(float x) {
+    return 1.f / (1.f + expf(-x));
+}
+
+
+Mat mat_alloc(size_t rows, size_t cols) {
+    Mat m;
+    m.rows = rows;
+    m.cols = cols;
+    m.stride = cols;
+    m.es = NN_MALLOC(sizeof(*m.es)*rows*cols);
+    NN_ASSERT(m.es != NULL);
+    return m;
+}
+
+void mat_dot(Mat dst, Mat a, Mat b) {
+    NN_ASSERT(a.cols == b.rows);
+    size_t n = a.cols;
+    NN_ASSERT(dst.rows == a.rows);
+    NN_ASSERT(dst.cols == b.cols);
+
+    for (size_t i = 0; i < dst.rows; ++i) {
+        for (size_t j = 0; j < dst.cols; ++j) {
+            MAT_AT(dst, i, j) = 0;
+            for (size_t k = 0; k < n; ++k) {
+                MAT_AT(dst, i, j) += MAT_AT(a, i, k) * MAT_AT(b, k, j);
+            }
+        }
+    }
+}
+
+void mat_sum(Mat dst, Mat a) {
+    NN_ASSERT(dst.rows == a.rows);
+    NN_ASSERT(dst.cols == a.cols);
+    for (size_t i = 0; i < dst.rows; ++i) {
+        for (size_t j = 0; j < dst.cols; ++j) {
+            MAT_AT(dst, i, j) += MAT_AT(a, i, j);
+        }
+    }
+}
+
+void mat_print(Mat m, const char *name) {
+    printf("%s = [\n", name);
+    for (size_t i = 0; i < m.rows; ++i) {
+        for (size_t j = 0; j < m.cols; ++j) {
+            printf("    %f ", MAT_AT(m, i, j));
+        }
+        printf("\n");
+    }
+    printf("]\n");
+}
+
+void mat_rand(Mat m, float low, float high) {
+    for (size_t i = 0; i < m.rows; ++i) {
+        for (size_t j = 0; j < m.cols; ++j) {
+            MAT_AT(m, i, j) = rand_float()*(high - low) + low;
+        }
+    }
+}
+
+void mat_fill(Mat m, float x) {
+    for (size_t i = 0; i < m.rows; ++i) {
+        for (size_t j = 0; j < m.cols; ++j) {
+            MAT_AT(m, i, j) = x;
+        }
+    }
+}
+
+void mat_sig(Mat m) {
+    for (size_t i = 0; i < m.rows; ++i) {
+        for (size_t j = 0; j < m.cols; ++j) {
+            MAT_AT(m, i, j) = sigmoidf(MAT_AT(m, i, j));
+        }
+    }
+}
+
+Mat mat_row(Mat m, size_t row) {
+    return (Mat) {
+        .rows = 1,
+        .cols = m.cols,
+        .stride = m.stride,
+        .es = &MAT_AT(m, row, 0),
+    };
+}
+void mat_col(Mat m, size_t col);
+void mat_copy(Mat dst, Mat src) {
+    NN_ASSERT(dst.rows == src.rows);
+    NN_ASSERT(dst.cols == src.cols);
+    for (size_t i = 0; i < dst.rows; ++i) {
+        for (size_t j = 0; j < dst.rows; ++j) {
+            MAT_AT(dst, i, j) = MAT_AT(src, i, j);
+        }
+
+    }
+}
+
+#endif
