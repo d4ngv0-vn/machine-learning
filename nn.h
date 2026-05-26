9341a67252ee53cccc9ee0e047c4600b6cfdc65a
d4ngv0-vn <d4ngv01303@gmail.com>
2026-05-26 13:38:29 +0700

    finish the framework

--- nn.h
+++ nn.h
@@ -15,6 +15,11 @@
 #define NN_ASSERT assert
 #endif // NN_ASSERT
 
+#define ARRAY_LEN(xs) sizeof(xs)/sizeof(xs[0])
+
+float rand_float(void);
+float sigmoidf(float x);
+
 typedef struct {
     size_t rows;
     size_t cols;
@@ -23,9 +28,6 @@ typedef struct {
 } Mat;
 
 #define MAT_AT(m, i, j) (m).es[(i)*(m).stride + (j)]
-
-float rand_float(void);
-float sigmoidf(float x);
 
 //allocate matrix with sizeof(*es)*rows*cols
 Mat mat_alloc(size_t rows, size_t cols);
@@ -37,8 +39,9 @@ void mat_fill(Mat m, float x);
 void mat_dot(Mat dst, Mat a, Mat b);
 //dst = dst + a
 void mat_sum(Mat dst, Mat a);
-//print m with its name
-void mat_print(Mat m, const char *name);
+//print m with its name and add padding
+void mat_print(Mat m, const char *name, size_t padding);
+#define MAT_PRINT(m) mat_print(m, #m, 0)
 //activate m with sigmoid
 void mat_sig(Mat m);
 //return the row of the matrix
@@ -47,8 +50,26 @@ void mat_col(Mat m, size_t col);
 //copy src to dst
 void mat_copy(Mat dst, Mat src);
 
-#define MAT_PRINT(m) mat_print(m, #m)
 
+typedef struct {
+    size_t count;
+    Mat *ws;
+    Mat *bs;
+    Mat *as; // count + 1
+} NN;
+
+#define NN_INPUT(nn) (nn).as[0]
+#define NN_OUTPUT(nn) (nn).as[(nn).count]
+
+
+NN nn_alloc(size_t *arch, size_t arch_count);
+void nn_print(NN nn, const char *name);
+#define NN_PRINT(nn) nn_print(nn, #nn)
+void nn_rand(NN nn, float low, float high);
+void nn_foward(NN nn);
+float nn_cost(NN nn, Mat in, Mat out);
+void nn_finite_diff(NN nn, NN g, float eps, Mat ti, Mat to);
+void nn_learn(NN nn, NN g, float rate);
 
 #endif // NN_H_
 
@@ -99,15 +120,16 @@ void mat_sum(Mat dst, Mat a) {
     }
 }
 
-void mat_print(Mat m, const char *name) {
-    printf("%s = [\n", name);
+void mat_print(Mat m, const char *name, size_t padding) {
+    printf("%*s%s = [\n", (int) padding, "", name);
     for (size_t i = 0; i < m.rows; ++i) {
+        printf("%*s    ",  (int) padding, "");
         for (size_t j = 0; j < m.cols; ++j) {
-            printf("    %f ", MAT_AT(m, i, j));
+            printf("%f ", MAT_AT(m, i, j));
         }
         printf("\n");
     }
-    printf("]\n");
+    printf("%*s]\n", (int) padding, "");
 }
 
 void mat_rand(Mat m, float low, float high) {
@@ -147,11 +169,133 @@ void mat_copy(Mat dst, Mat src) {
     NN_ASSERT(dst.rows == src.rows);
     NN_ASSERT(dst.cols == src.cols);
     for (size_t i = 0; i < dst.rows; ++i) {
-        for (size_t j = 0; j < dst.rows; ++j) {
+        for (size_t j = 0; j < dst.cols; ++j) {
             MAT_AT(dst, i, j) = MAT_AT(src, i, j);
         }
 
     }
 }
+
+NN nn_alloc(size_t *arch, size_t arch_count) {
+    NN_ASSERT(arch_count > 0);
+    NN nn;
+    nn.count = arch_count - 1;
+    
+    nn.ws = NN_MALLOC(sizeof(*nn.ws)*nn.count);
+    NN_ASSERT(nn.ws != NULL);
+    nn.bs = NN_MALLOC(sizeof(*nn.bs)*nn.count);
+    NN_ASSERT(nn.bs != NULL);
+    nn.as = NN_MALLOC(sizeof(*nn.as)*(nn.count + 1));
+    NN_ASSERT(nn.as != NULL);
+    
+    nn.as[0] = mat_alloc(1, arch[0]);
+
+    for (size_t i = 1; i < arch_count; ++i) {
+        nn.ws[i - 1] = mat_alloc(nn.as[i - 1].cols, arch[i]);
+        nn.bs[i - 1] = mat_alloc(1, arch[i]);
+        nn.as[i]     = mat_alloc(1, arch[i]);
+    }
+    
+    return nn;
+}
+
+void nn_print(NN nn, const char *name) {
+    char buf[256];
+    printf("%s = [\n", name);
+    // Mat *ws = nn.ws;
+    // Mat *bs = nn.bs;
+    for (size_t i = 0; i < nn.count; ++i) {
+        snprintf(buf, sizeof(buf), "ws%zu", i);
+        mat_print(nn.ws[i], buf, 4);
+        snprintf(buf, sizeof(buf), "bs%zu", i);
+        mat_print(nn.bs[i], buf, 4);
+    }
+    printf("]\n");
+}
+
+void nn_rand(NN nn, float low, float high) {
+    for (size_t i = 0; i < nn.count; ++i) {
+        mat_rand(nn.ws[i], low, high);   
+        mat_rand(nn.bs[i], low, high);   
+
+    }
+}
+
+void nn_foward(NN nn) {
+    for (size_t i = 0; i < nn.count; ++i) {
+        mat_dot(nn.as[i + 1], nn.as[i], nn.ws[i]);
+        mat_sum(nn.as[i + 1], nn.bs[i]);
+        mat_sig(nn.as[i + 1]);
+    }
+}
+
+float nn_cost(NN nn, Mat ti, Mat to) {
+    NN_ASSERT(ti.rows == to.rows);
+    NN_ASSERT(to.cols == NN_OUTPUT(nn).cols);
+
+    size_t n = ti.rows;
+    
+    float c = 0;
+    for (size_t i = 0; i < n; ++i) {
+        Mat x = mat_row(ti, i);
+        Mat y = mat_row(to, i);
+        
+        mat_copy(NN_INPUT(nn), x);      
+        nn_foward(nn);
+        
+        size_t q = to.cols;
+
+        for (size_t j = 0; j < q; ++j) {
+            float d = MAT_AT(NN_OUTPUT(nn), 0, j) - MAT_AT(y, 0, j);
+            c += d*d;
+        }
+
+    }
+
+    return c/n;
+}
+
+
+void nn_finite_diff(NN nn, NN g, float eps, Mat ti, Mat to) {
+    float saved;
+
+    float c = nn_cost(nn, ti, to);
+    for (size_t i = 0; i < nn.count; ++i) {
+        for (size_t j = 0; j < nn.ws[i].rows; ++j) {
+            for (size_t k = 0; k < nn.ws[i].cols; ++k) {
+                saved = MAT_AT(nn.ws[i], j, k);
+                MAT_AT(nn.ws[i], j, k) += eps;
+                MAT_AT(g.ws[i], j, k) = (nn_cost(nn, ti, to) - c)/eps;
+                MAT_AT(nn.ws[i], j, k) = saved;
+            }
+        }
+
+        for (size_t j = 0; j < nn.bs[i].rows; ++j) {
+            for (size_t k = 0; k < nn.bs[i].cols; ++k) {
+                saved = MAT_AT(nn.bs[i], j, k);
+                MAT_AT(nn.bs[i], j, k) += eps;
+                MAT_AT(g.bs[i], j, k) = (nn_cost(nn, ti, to) - c)/eps;
+                MAT_AT(nn.bs[i], j, k) = saved;
+            }
+        }
+    }
+}
+
+void nn_learn(NN nn, NN g, float rate) {
+    for (size_t i = 0; i < nn.count; ++i) {
+        for (size_t j = 0; j < nn.ws[i].rows; ++j) {
+            for (size_t k = 0; k < nn.ws[i].cols; ++k) {
+                MAT_AT(nn.ws[i], j, k) -= MAT_AT(g.ws[i], j, k) * rate;
+            }
+        }
+        
+        for (size_t j = 0; j < nn.bs[i].rows; ++j) {
+            for (size_t k = 0; k < nn.bs[i].cols; ++k) {
+                MAT_AT(nn.bs[i], j, k) -= MAT_AT(g.bs[i], j, k) * rate;
+            }
+        }
+    }
+}
+
 
 #endif
